d1e5dcfb7138f3eff4fab49ac04af146affc3b13
DanielMa <mwldaniel_008@126.com>
2023-09-02 06:27:17 +0800

    manipulating DOM with javascript

--- script.js
+++ script.js
@@ -8,8 +8,22 @@ let apiQuotes = [];
 
 // Show new Quote
 function newQuote() {
+  // Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-  authorText.textContent = quote.author;
+  // Check if Author field is blank and replace it with 'Unknown'
+  if (!quote.author) {
+    authorText.textContent = 'Unknown'
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  // Check Quote length to determine styling
+  if (quote.text.length > 120) {
+    quoteText.classList.add('long-quote')
+  } else {
+    quoteText.classList.remove('long-quote')
+  }
+
   quoteText.textContent = quote.text;
 }
 
@@ -24,6 +38,16 @@ async function getQuotes() {
     
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote)
+twitterBtn.addEventListener('click', tweetQuote)
 
 // On Load
 getQuotes()
